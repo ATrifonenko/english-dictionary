e0a2a36338dd344d8a288651e51d5406452a0187
Andrey Trifonenko <sintez91@gmail.com>
2021-03-28 15:30:16 +0500

    validation

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -4,10 +4,34 @@ import './Auth.css';
 function Auth(props) {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
 
   const submit = (e) => {
     e.preventDefault();
-    props.submit(login, password);
+    const validationErrors = validate(login, password);
+    if (Object.entries(validationErrors).length === 0) {
+      setErrors({});
+      props.submit(login, password);
+    } else {
+      setErrors(validationErrors);
+    }
+  };
+
+  const validate = (login, password) => {
+    const validationErrors = {};
+    if (
+      !login ||
+      !/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(
+        login
+      )
+    ) {
+      validationErrors.login = 'Не валидный e-mail';
+    }
+    if (!password || !/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{7,})\S$/.test(password)) {
+      validationErrors.password =
+        'Пароль должен содержать не менее восьми знаков, включать заглавные и строчные буквы, цифры';
+    }
+    return validationErrors;
   };
 
   return (
@@ -25,6 +49,7 @@ function Auth(props) {
           value={login}
           id="email"
         />
+        {errors.login ? <p className="auth-form__error-description">{errors.login}</p> : ''}
         <label className="auth-form__lable" htmlFor="pass">
           Пароль:
         </label>
@@ -36,6 +61,7 @@ function Auth(props) {
           value={password}
           id="pass"
         />
+        {errors.password ? <p className="auth-form__error-description">{errors.password}</p> : ''}
         <button className="auth-form__btn-submit button" type="submit" onClick={submit}>
           {props.title}
         </button>
